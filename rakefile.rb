f16223b23d08b7c3afbc21cd31983c392a731072
Joshua Flanagan <joshuaflanagan@gmail.com>
2011-03-05 10:37:35 -0600

    Default rake target now builds, tests, and packages for .NET 4 and .NET 3.5
    - version is tracked in VERSION.txt and is used to determine
      assembly version and Nuget package version

--- rakefile.rb
+++ rakefile.rb
@@ -1,32 +1,34 @@
-COMPILE_TARGET = ENV['config'].nil? ? "debug" : ENV['config']
+COMPILE_TARGET = ENV['config'].nil? ? "release" : ENV['config']
 
 include FileTest
 require 'albacore'
+load "VERSION.txt"
 
 RESULTS_DIR = "results"
-BUILD_NUMBER_BASE = "0.8.0"
 PRODUCT = "FubuMVC"
 COPYRIGHT = 'Copyright 2009-2011 Jeremy D. Miller, et al. All rights reserved.';
 COMMON_ASSEMBLY_INFO = 'src/CommonAssemblyInfo.cs';
 CLR_TOOLS_VERSION = "v4.0.30319"
 
+build_number = "#{BUILD_VERSION}.#{Date.today.strftime('%y%j')}"
+
 props = { :stage => File.expand_path("build"), :stage35 => File.expand_path("build35"), :artifacts => File.expand_path("artifacts") }
 
 desc "**Default**, compiles and runs tests"
-task :default => [:compile, :unit_test]
+task :default => [:compile, :unit_test, :stage, "fx35:compile", "fx35:unit_test", "fx35:stage", "package:nuget"]
 
 desc "Update the version information for the build"
 assemblyinfo :version do |asm|
-  asm_version = BUILD_NUMBER_BASE + ".0"
+  asm_version = BUILD_VERSION + ".0"
   
   begin
     commit = `git log -1 --pretty=format:%H`
   rescue
     commit = "git unavailable"
   end
-  build_number = "#{BUILD_NUMBER_BASE}.#{Date.today.strftime('%y%j')}"
   tc_build_number = ENV["BUILD_NUMBER"]
   puts "##teamcity[buildNumber '#{build_number}-#{tc_build_number}']" unless tc_build_number.nil?
+  puts "Version: #{build_number}" if tc_build_number.nil?
   asm.trademark = commit
   asm.product_name = PRODUCT
   asm.description = build_number
@@ -75,6 +77,12 @@ nunit :unit_test do |nunit|
   nunit.assemblies = ["src/HtmlTags.Testing/bin/#{COMPILE_TARGET}/HtmlTags.Testing.dll"]
 end
 
+task :stage do
+  Dir.glob "src/HtmlTags/bin/#{COMPILE_TARGET}/HtmlTags.*" do |f|
+    cp f, props[:stage]
+  end
+end
+
 namespace :fx35 do
   desc "Compile the app against the .NET Framework 3.5"
   msbuild :compile do |msb|
@@ -85,11 +93,23 @@ namespace :fx35 do
     msb.targets :Rebuild
     msb.log_level = :verbose
   end
+
+  desc "Run unit tests against the .NET Framework 3.5"
+  nunit :unit_test do |nunit|
+    nunit.path_to_command = 'lib/nunit/nunit-console.exe'
+    nunit.assemblies = ["src/HtmlTags.Testing/bin/#{COMPILE_TARGET}/HtmlTags.Testing.dll"]
+  end
+
+  task :stage do
+    Dir.glob "src/HtmlTags/bin/#{COMPILE_TARGET}35/HtmlTags.*" do |f|
+      cp f, props[:stage35]
+    end
+  end
 end
 
 namespace :package do
   desc "Build nuget package"
   task :nuget do
-    sh "lib/nuget.exe pack packaging/nuget/htmltags.nuspec -o #{props[:artifacts]}"
+    sh "lib/nuget.exe pack packaging/nuget/htmltags.nuspec -o #{props[:artifacts]} -Version #{build_number}"
   end
 end
