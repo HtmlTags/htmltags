2f9f03825f23ac282a681186bc03a1936f2a3fe6
Adam Schroder <adamschroder@gmail.com>
2012-07-14 13:01:39 +1000

    Implement Parent property and ability to render from upmost parent

---
+++ src/HtmlTags.Testing/ParentTagTester.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FubuTestingSupport;
+using NUnit.Framework;
+
+namespace HtmlTags.Testing
+{
+    [TestFixture]
+    public class ParentTagTester
+    {
+        [Test]
+        public void parent_property_is_set_correctly()
+        {
+            var tag = new HtmlTag("div");
+            var child = tag.Add("span");
+            tag.ShouldEqual(child.Parent);
+            tag.Children[0].ShouldEqual(child);
+        }
+
+        [Test]
+        public void render_html_from_current_tag_by_default()
+        {
+            var tag = new HtmlTag("div");
+            var child = tag.Add("span").Text("hi");
+            child.ToString().ShouldEqual("<span>hi</span>");
+        }
+    
+        [Test]
+        public void render_html_from_top_if_set_renderfromtop()
+        {
+            var tag = new HtmlTag("div");
+            var child = tag.Add("span").RenderFromTop().Text("hi");
+            child.ToString().ShouldEqual("<div><span>hi</span></div>");
+        }
+    }
+}
