2062c115f877bff62fbbd9741c87859a00c9ac80
Jeremy Miller <jeremydmiller@yahoo.com>
2012-09-11 10:03:48 -0500

    TagLibrary.Import()

--- src/HtmlTags.Testing/Conventions/TagLibraryTester.cs
+++ src/HtmlTags.Testing/Conventions/TagLibraryTester.cs
@@ -1,6 +1,7 @@
 using HtmlTags.Conventions;
 using NUnit.Framework;
 using FubuTestingSupport;
+using Rhino.Mocks;
 
 namespace HtmlTags.Testing.Conventions
 {
@@ -25,11 +26,11 @@ namespace HtmlTags.Testing.Conventions
         public void builds_default_if_no_category_or_profile_is_specified()
         {
             theLibrary.Always.Build(x => new HtmlTag("div").Text(x.Name));
-            theLibrary.ForCategory("a").Always.Build(x => new HtmlTag("a").Text(x.Name));
-            theLibrary.ForCategory("b").Always.Build(x => new HtmlTag("b").Text(x.Name));
+            theLibrary.Category("a").Always.Build(x => new HtmlTag("a").Text(x.Name));
+            theLibrary.Category("b").Always.Build(x => new HtmlTag("b").Text(x.Name));
         
             theLibrary.ForProfile("profile1").Always.Build(x => new HtmlTag("p").Text(x.Name));
-            theLibrary.ForCategory("a").ForProfile("a-1").Always.Modify(x => x.CurrentTag.AddClass("a-1"));
+            theLibrary.Category("a").ForProfile("a-1").Always.Modify(x => x.CurrentTag.AddClass("a-1"));
 
             var subject = new FakeSubject { Name = "Lindsey" };
 
@@ -39,5 +40,90 @@ namespace HtmlTags.Testing.Conventions
             build(subject, category:"b").ToString().ShouldEqual("<b>Lindsey</b>");
             build(subject, profile:"profile1").ToString().ShouldEqual("<p>Lindsey</p>");
         }
+
+
+        
+    }
+
+    [TestFixture]
+    public class TagLibrary_Import_Tester
+    {
+        private ITagBuilder<FakeSubject> b1;
+        private ITagBuilder<FakeSubject> b2;
+        private ITagBuilder<FakeSubject> b3;
+        private ITagBuilder<FakeSubject> b4;
+        private ITagBuilder<FakeSubject> b5;
+        private ITagBuilder<FakeSubject> b6;
+        private ITagBuilder<FakeSubject> b7;
+        private ITagBuilder<FakeSubject> b8;
+        private ITagModifier<FakeSubject> m1;
+        private ITagModifier<FakeSubject> m2;
+        private ITagModifier<FakeSubject> m3;
+        private ITagModifier<FakeSubject> m4;
+        private ITagModifier<FakeSubject> m5;
+        private TagLibrary<FakeSubject> library1;
+
+        [SetUp]
+        public void SetUp()
+        {
+            b1 = MockRepository.GenerateMock<ITagBuilder<FakeSubject>>();
+            b2 = MockRepository.GenerateMock<ITagBuilder<FakeSubject>>();
+            b3 = MockRepository.GenerateMock<ITagBuilder<FakeSubject>>();
+            b4 = MockRepository.GenerateMock<ITagBuilder<FakeSubject>>();
+            b5 = MockRepository.GenerateMock<ITagBuilder<FakeSubject>>();
+            b6 = MockRepository.GenerateMock<ITagBuilder<FakeSubject>>();
+            b7 = MockRepository.GenerateMock<ITagBuilder<FakeSubject>>();
+            b8 = MockRepository.GenerateMock<ITagBuilder<FakeSubject>>();
+
+            m1 = MockRepository.GenerateMock<ITagModifier<FakeSubject>>();
+            m2 = MockRepository.GenerateMock<ITagModifier<FakeSubject>>();
+            m3 = MockRepository.GenerateMock<ITagModifier<FakeSubject>>();
+            m4 = MockRepository.GenerateMock<ITagModifier<FakeSubject>>();
+            m5 = MockRepository.GenerateMock<ITagModifier<FakeSubject>>();
+
+            library1 = new TagLibrary<FakeSubject>();
+
+
+            library1.Add(b1);
+            library1.Add(m1);
+
+            library1.Default.Profile("A").Add(b2);
+            library1.Default.Profile("A").Add(m2);
+
+            library1.Category("Cat1").Add(b3);
+            library1.Category("Cat1").Add(m3);
+
+            library1.Category("Cat1").Profile("A").Add(b4);
+
+
+            var library2 = new TagLibrary<FakeSubject>();
+            library2.Add(b5);
+            library2.Add(m4);
+
+            library2.Category("Cat2").Add(b6);
+            library2.Category("Cat1").Add(b7);
+
+            library1.Import(library2);
+        }
+
+        [Test]
+        public void imports_defaults()
+        {
+            library1.Default.Defaults.Builders.ShouldHaveTheSameElementsAs(b1, b5);
+        }
+
+        [Test]
+        public void imports_category_that_both_libraries_have()
+        {
+            library1.Category("Cat1").Defaults.Builders.ShouldHaveTheSameElementsAs(b3, b7);
+        }
+
+        [Test]
+        public void imports_category_from_the_second_library_not_originally_in_the_first()
+        {
+            library1.Category("Cat2").Defaults.Builders.ShouldHaveTheSameElementsAs(b6);
+        }
+
+
     }
 }
--- src/HtmlTags/Conventions/TagLibrary.cs
+++ src/HtmlTags/Conventions/TagLibrary.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 
 namespace HtmlTags.Conventions
 {
@@ -30,24 +31,39 @@ namespace HtmlTags.Conventions
             return _categories[TagConstants.Default].If(matches);
         }
 
-        public TagCategory<T> ForCategory(string category)
+        public TagCategory<T> Category(string category)
         {
             return _categories[category];
         }
 
         public void Add(ITagBuilder<T> builder)
         {
-            _categories[TagConstants.Default].Add(builder);
+            Default.Add(builder);
         }
 
         public void Add(ITagModifier<T> modifier)
         {
-            _categories[TagConstants.Default].Add(modifier);
+            Default.Add(modifier);
+        }
+
+        public TagCategory<T> Default
+        {
+            get
+            {
+                return _categories[TagConstants.Default];
+            }
         }
 
         public ITagBuildingExpression<T> ForProfile(string profile)
         {
             return _categories[TagConstants.Default].ForProfile(profile);
+        }
+
+        public void Import(TagLibrary<T> other)
+        {
+            var keys = _categories.GetKeys().Union(other._categories.GetKeys()).Distinct();
+
+            keys.Each(key => _categories[key].Import(other._categories[key]));
         }
     }
 }
