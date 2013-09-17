d6ee5f968c791dcb1a06b3ff0696a0fd17cd3d4b
Jimmy Bogard <jimmy.bogard@gmail.com>
2013-09-17 13:33:41 -0500

    Failing test showing can't select value by its raw value

--- src/HtmlTags.Testing/SelectTagTester.cs
+++ src/HtmlTags.Testing/SelectTagTester.cs
@@ -23,6 +23,21 @@ namespace HtmlTags.Testing
         }
 
         [Test]
+        public void selected_by_string_value()
+        {
+            var tag = new SelectTag();
+            tag.Option("a", 1);
+            tag.Option("b", 2);
+            tag.Option("c", 3);
+
+            tag.SelectByValue(2);
+
+            tag.Children[0].HasAttr("selected").ShouldBeFalse();
+            tag.Children[1].Attr("selected").ShouldEqual("selected");
+            tag.Children[2].HasAttr("selected").ShouldBeFalse();
+        }
+
+        [Test]
         public void can_initialize_options_in_constructor()
         {
             var select = new SelectTag(tag =>
