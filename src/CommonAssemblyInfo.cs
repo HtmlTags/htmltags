be89186621469395b0d7cb2417f01be74f4e5a3f
Kendall Bennett <kendallb@amain.com>
2014-04-07 17:40:05 -0700

    Updates assembly info for 2.1.0

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -1,9 +1,9 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
-[assembly: AssemblyDescription("2.0.0.51355")]
+[assembly: AssemblyDescription("2.1.0.51737")]
 [assembly: AssemblyProduct("HtmlTags")]
 [assembly: AssemblyCopyright("Copyright 2008-2013 Jeremy D. Miller, Josh Arnold, Joshua Flanagan, et al. All rights reserved.")]
-[assembly: AssemblyTrademark("30cc33160ba93091ea8727dbaffa0d49a72c92a5")]
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.51355")]
-[assembly: AssemblyInformationalVersion("2.0.0.0")]
+[assembly: AssemblyTrademark("2c7d306b4170f16a64fcf0459124673a8b087a0b")]
+[assembly: AssemblyVersion("2.1.0.0")]
+[assembly: AssemblyFileVersion("2.1.0.51737")]
+[assembly: AssemblyInformationalVersion("2.1.0.0")]
