138e8430919a86d495460b06c6d49779323672f7
Kendall Bennett <kendallb@amain.com>
2014-04-07 17:29:28 -0700

    Changed to include CommonAssemblyInfo.cs so we do not need to generate it. Makes it easier to use as a subtree in Git.

---
+++ src/CommonAssemblyInfo.cs
@@ -0,0 +1,9 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+[assembly: AssemblyDescription("2.0.0.51355")]
+[assembly: AssemblyProduct("HtmlTags")]
+[assembly: AssemblyCopyright("Copyright 2008-2013 Jeremy D. Miller, Josh Arnold, Joshua Flanagan, et al. All rights reserved.")]
+[assembly: AssemblyTrademark("30cc33160ba93091ea8727dbaffa0d49a72c92a5")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.51355")]
+[assembly: AssemblyInformationalVersion("2.0.0.0")]
