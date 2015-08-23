3897f700e4b8cad56d4e4afbed7107007de1e885
Mihai Mogosanu <mike@sapiensworks.com>
2015-08-23 13:59:58 -0400

    added several new tags and extensions methods
    tags added: Label, Textarea, RadioButton

--- src/HtmlTags/HtmlTagExtensions.cs
+++ src/HtmlTags/HtmlTagExtensions.cs
@@ -8,5 +8,53 @@ namespace HtmlTags
         {
             return new TagList(tags);
         }
+
+        public static HtmlTag EmailMode(this HtmlTag tag)
+        {
+            return tag.Attr("type", "email");
+        }
+
+        /// <summary>
+        /// Sets the input as checked (checkbox, radio buttons)
+        /// </summary>
+        /// <param name="tag"></param>
+        /// <returns></returns>
+        public static T Checked<T>(this T tag) where T : HtmlTag
+        {
+            if (!tag.IsInputElement()) return tag;
+            tag.Attr("checked", "checked");
+            return tag;
+        }
+
+        public static FormTag DoPOST(this FormTag tag)
+        {
+            return tag.Method("POST");
+        }
+
+        public static FormTag DoGET(this FormTag tag)
+        {
+            return tag.Method("GET");
+        }
+
+        public static string IdFromName(string name)
+        {
+           return name?.Replace('.', '_');
+        }
+
+        /// <summary>
+        /// Sets the id of the tag using the asp.net mvc default convention
+        /// </summary>
+        /// <param name="tag"></param>
+        /// <returns></returns>
+        public static T IdFromName<T>(this T tag) where T : HtmlTag
+        {
+            tag.Id(IdFromName(tag.Attr("name")));
+            return tag;
+        }
+
+        public static LabelTag CreateLabel(this HtmlTag tag, string display)
+        {
+            return new LabelTag(tag.Id(), display);
+        }
     }
 }
