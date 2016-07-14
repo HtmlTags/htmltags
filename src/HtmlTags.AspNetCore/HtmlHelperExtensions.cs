a90728409e680e78d63d15b3cb3929da5c64d328
Jimmy Bogard <jimmy.bogard@gmail.com>
2016-07-14 15:24:50 -0500

    Html helper extensions

---
+++ src/HtmlTags.AspNetCore/HtmlHelperExtensions.cs
@@ -0,0 +1,47 @@
+﻿namespace HtmlTags
+{
+    using System;
+    using System.Linq.Expressions;
+    using Conventions;
+    using Conventions.Elements;
+    using Microsoft.AspNetCore.Mvc.Rendering;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public static class HtmlHelperExtensions
+    {
+        public static HtmlTag Input<T>(this IHtmlHelper<T> helper, Expression<Func<T, object>> expression)
+            where T : class
+        {
+            var generator = GetGenerator(helper);
+            return generator.InputFor(expression);
+        }
+
+        public static HtmlTag Label<T>(this IHtmlHelper<T> helper, Expression<Func<T, object>> expression)
+            where T : class
+        {
+            var generator = GetGenerator(helper);
+            return generator.LabelFor(expression);
+        }
+
+        public static HtmlTag Display<T>(this IHtmlHelper<T> helper, Expression<Func<T, object>> expression)
+            where T : class
+        {
+            var generator = GetGenerator(helper);
+            return generator.DisplayFor(expression);
+        }
+
+        public static HtmlTag Tag<T>(this IHtmlHelper<T> helper, Expression<Func<T, object>> expression, string category)
+            where T : class
+        {
+            var generator = GetGenerator(helper);
+            return generator.TagFor(expression, category);
+        }
+
+        public static IElementGenerator<T> GetGenerator<T>(IHtmlHelper<T> helper) where T : class
+        {
+            var library = helper.ViewContext.HttpContext.RequestServices.GetService<HtmlConventionLibrary>();
+            return ElementGenerator<T>.For(library, t => helper.ViewContext.HttpContext.RequestServices.GetService(t), helper.ViewData.Model);
+        }
+
+    }
+}
---
+++ src/HtmlTags.AspNetCore/ServiceCollectionExtensions.cs
@@ -0,0 +1,33 @@
+﻿namespace HtmlTags
+{
+    using System;
+    using Conventions;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public static class ServiceCollectionExtensions
+    {
+        public static void AddHtmlTags(this IServiceCollection services, HtmlConventionLibrary library)
+        {
+            services.AddSingleton(library);
+        }
+
+        public static void AddHtmlTags(this IServiceCollection services, params HtmlConventionRegistry[] registries)
+        {
+            var library = new HtmlConventionLibrary();
+            foreach (var registry in registries)
+            {
+                registry.Apply(library);
+            }
+            services.AddHtmlTags(library);
+        }
+
+        public static void AddHtmlTags(this IServiceCollection services, Action<HtmlConventionRegistry> config)
+        {
+            var registry = new HtmlConventionRegistry();
+
+            config(registry);
+
+            services.AddHtmlTags(registry);
+        }
+    }
+}
