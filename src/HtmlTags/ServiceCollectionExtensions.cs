4696096c196fac1549a7bc0ab0936a379bb65a9f
Jimmy Bogard <jimmy.bogard@gmail.com>
2020-12-31 08:14:12 -0600

    Correcting namespace

--- src/HtmlTags/ServiceCollectionExtensions.cs
+++ src/HtmlTags/ServiceCollectionExtensions.cs
@@ -1,8 +1,8 @@
-﻿namespace HtmlTags
+﻿namespace Microsoft.Extensions.DependencyInjection
 {
     using System;
-    using Conventions;
-    using Microsoft.Extensions.DependencyInjection;
+    using HtmlTags;
+    using HtmlTags.Conventions;
 
     public static class ServiceCollectionExtensions
     {
